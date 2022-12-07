0dafaa4323bb97916b3a1427db40ad04bed25934
Rahul Kumar <rahul.kumar@swastik.abmindia.com>
2022-12-07 17:49:37 +0530

    login and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,21 @@ import { SignupComponent } from './pages/signup/signup.component';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule } from "@angular/forms";
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MatCardModule } from '@angular/material/card';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,8 +32,10 @@ import { FormsModule } from "@angular/forms";
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
-    FormsModule
-  
+    FormsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
